b2d47c3e2e4e1289c6d61874357afc24e4142551
Poorna Rao <mv.poorna@gmail.com>
2015-10-22 16:44:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<<!DOCTYPE html>
-<html>
-<head>
-	<title>This is my First Application on Git</title>
-</head>
-<body>
+<?php
+   include("header.html");
+?>
 	<!-- This is My first web form -->
 	<h1>Welcome to my repository</h1>
 </body>
